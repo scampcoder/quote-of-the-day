1995441a5f19b92ce262bcc58cd1ba8cca19e565
Victoria Kee <victoriakee95@gmail.com>
2020-05-09 17:17:21 -0500

    Add quote dictionary

---
+++ quotes.js
@@ -0,0 +1,110 @@
+const quotes = [
+  {
+    "author": "Oscar Wilde",
+    "quote": "Be yourself; everyone else is already taken."
+  },
+  {
+    "author": "Bernard M Baruch",
+    "quote": "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind."
+  },
+  {
+    "author": "Marcus Tullius Cicero",
+    "quote": "A room without books is like a body without a soul."
+  },
+  {
+    "author": "Mae West",
+    "quote": "You only live once, but if you do it right, once is enough."
+  },
+  {
+    "author": "Eleanor Roosevelt",
+    "quote": "No one can make you feel inferior without your consent."
+  },
+  {
+    "author": "Oscar Wilde",
+    "quote": "Always forgive your enemies; nothing annoys them so much."
+  },
+  {
+    "author": "Mahatma Gandhi",
+    "quote": "Live as if you were to die tomorrow. Learn as if you were to live forever."
+  },
+  {
+    "author": "Martin Luther King Jr.",
+    "quote": "Hate cannot drive out hate: only love can do that."
+  },
+  {
+    "author": "Stephen Chbosky",
+    "quote": "We accept the love we think we deserve."
+  },
+  {
+    "author": "Rob Siltanen",
+    "quote": "Here's to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius. Because the people who are crazy enough to think they can change the world, are the ones who do."
+  },
+  {
+    "author": "Ralph Waldo Emerson",
+    "quote": "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment."
+  },
+  {
+    "author": "Marilyn Monroe",
+    "quote": "I believe that everything happens for a reason. People change so that you can learn to let go, things go wrong so that you appreciate them when they're right, you believe lies so you eventually learn to trust no one but yourself, and sometimes good things fall apart so better things can fall together."
+  },
+  {
+    "author": "H. Jackson Brown Jr.",
+    "quote": "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover."
+  },
+  {
+    "author": "Marilyn Monroe",
+    "quote": "Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring."
+  },
+  {
+    "author": "Albert Einstein",
+    "quote": "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."
+  },
+  {
+    "author": "Maurice Switzer",
+    "quote": "It is better to remain silent at the risk of being thought a fool, than to talk and remove all doubt of it."
+  },
+  {
+    "author": "Grandma Aggie, Halloweentown",
+    "quote": "Being normal is vastly overrated."
+  },
+  {
+    "author": "Blake Lively",
+    "quote": "The most important thing you can ever wear is confidence."
+  },
+  {
+    "author": "Unknown",
+    "quote": "Do what scares you, because fear is a liar, y'all."
+  },
+  {
+    "author": "G.D. Anderson",
+    "quote": "Feminism isn’t about making women strong. Women are already strong. It’s about changing the way the world perceives that strength."
+  },
+  {
+    "author": "George Harrison",
+    "quote": "It’s being here now that’s important. There’s no past and there’s no future. Time is a very misleading thing. All there is ever, is the now. We can gain experience from the past, but we can’t relive it; and we can hope for the future, but we don’t know if there is one."
+  },
+  {
+    "author": "Unknown",
+    "quote": "Always find time for the things that make you happy to be alive."
+  },
+  {
+    "author": "Kathleen Winter",
+    "quote": "It is not fair to treat people as if they are finished beings. Everyone is always becoming and unbecoming."
+  },
+  {
+    "author": "Socrates",
+    "quote": "If you don’t get what you want, you suffer; if you get what you don’t want, you suffer; even when you get exactly what you want, you still suffer because you can’t hold on to it forever. Your mind is your predicament. It wants to be free of change. Free of pain, free of the obligations of life and death. But change is law and no amount of pretending will alter that reality."
+  },
+  {
+    "author": "Kurt Vonnegut",
+    "quote": "Enjoy your body, use it every way you can. Don’t be afraid of it, or what other people think of it, it’s the greatest instrument you’ll ever own."
+  },
+  {
+    "author": "Unknown",
+    "quote": "Another person's success is not your failure."
+  },{
+    "author": "Dr. Frankenfurter, Rocky Horror Picture Show"
+    "quote": "Don't dream it. Be it."
+  }
+
+]
